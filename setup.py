2e9c87bce52e5a236cf75f71f81f09eb5a8a5a54
Hunter Zolomon <24488175+Hunter-Zolomon@users.noreply.github.com>
2022-08-08 20:41:13 +0800

    Library version bump
    Bump and lock lib versions
    
    Fix version notation
    
    Revert lib versions. Maintain mujoco version.
    
    Assign package version
    
    Explicitly set packages
    
    Replace packages with package finder
    
    Include package data
    
    Bump package version
    
    Fix observation box bounds
    
    Bump gym version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 
 assert sys.version_info.major == 3 and sys.version_info.minor >= 6, \
@@ -9,12 +9,14 @@ assert sys.version_info.major == 3 and sys.version_info.minor >= 6, \
 
 setup(
     name='safety_gym',
-    packages=['safety_gym'],
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'gym~=0.15.3',
-        'joblib~=0.14.0',
-        'mujoco_py==2.0.2.7',
-        'numpy~=1.17.4',
+        'gym~=0.19.0',
+        'joblib~=1.0.1',
+        'mujoco_py',
+        'numpy~=1.21.6',
         'xmltodict~=0.12.0',
     ],
 )
